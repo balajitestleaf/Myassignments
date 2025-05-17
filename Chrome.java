8ef8e438605a5c0fdde279803514b16147df05b9
balajitestleaf <balagiridharan.g@gmail.com>
2025-05-17 20:56:46 +0530

    Add files via upload
    week 1 day 1 assignment

---
+++ Chrome.java
@@ -0,0 +1,22 @@
+package day1myassignments;
+
+public class Chrome {
+
+	public static void main(String[] args)
+	
+	{
+		
+         Float versionofthetype = 91.0f;
+         String developerofthetype = "Google";
+         boolean isBetaoftype = false;
+         int releaseyearoftype = 2008;
+         char shortcutKeyoftype = 'C';
+         System.out.println("version of the type" +versionofthetype);
+         System.out.println("developer of the type" +developerofthetype);
+         System.out.println("is beta of the type" +isBetaoftype);
+         System.out.println("release year of the type" +releaseyearoftype);		 
+         System.out.println("shortcutkey of the type" +shortcutKeyoftype);
+        
+	}
+
+}
---
+++ Positivenumber.java
@@ -0,0 +1,26 @@
+package day1myassignments;
+
+public class Positivenumber
+
+{
+
+	public static void main(String[] args) 
+	
+	
+	{
+		int version=10;
+		if(version>0)
+		{
+			System.out.println("positive number");
+		}
+		else
+		{
+			System.out.println("negative number");
+			
+		}
+		
+		
+
+	}
+
+}
