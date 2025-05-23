2d1445d61055fce1bcd9ae27bebd35ec3f24233a
balajitestleaf <balagiridharan.g@gmail.com>
2025-05-23 17:19:40 +0530

    Add files via upload
    week 1 day 2 assignment

---
+++ Primenumber.java
@@ -0,0 +1,40 @@
+package javaprograms;
+
+public class Primenumber 
+{
+
+	public static void main(String[] args)
+	
+	{
+	
+		
+			
+
+			int n=97;
+			for(int i=2;i<=n-1;i++)
+				
+			{
+				if(i/n==0)
+					
+					
+					
+				{
+					System.out.println("it is prime number" +n);
+				}
+				
+				else
+					
+				{
+					System.out.println("it is not a prime number" +n);
+					
+				}
+			}
+			
+			
+		}
+
+		}
+
+	
+
+
